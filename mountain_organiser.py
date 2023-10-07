a20e9fee10c80e5953e0abdd9e066b708e56403d
muchralt <umya0001@student.monash.edu>
2023-10-07 23:27:41 +1100

    test

--- mountain_organiser.py
+++ mountain_organiser.py
@@ -1,14 +1,18 @@
 from __future__ import annotations
+from algorithms.mergesort import mergesort, merge
+from algorithms.binary_search import binary_search
 
 from mountain import Mountain
 
 class MountainOrganiser:
 
     def __init__(self) -> None:
-        raise NotImplementedError()
+        self.mountains_list = []
 
     def cur_position(self, mountain: Mountain) -> int:
-        raise NotImplementedError()
-
+        return binary_search(self.mountains_list, mountain.difficulty_level)
+        
     def add_mountains(self, mountains: list[Mountain]) -> None:
-        raise NotImplementedError()
+        for mountain in mountains:
+            input_list = mergesort(mountains, key=lambda x:x.difficulty_level)
+        self.mountains_list = merge(self.mountains_list, input_list, key=lambda x:x.difficulty_level)
